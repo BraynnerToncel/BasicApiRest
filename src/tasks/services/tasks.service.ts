75cb0cceedeb8bde7451cb1e60c829d09cb3a353
unknown <braynnert31@gmal.com>
2024-05-09 10:43:51 -0500

    actualización de modulos

--- src/tasks/services/tasks.service.ts
+++ src/tasks/services/tasks.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from '../entities/task.entity';
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(Task) private tasksRepository: Repository<Task>,
+  ) {}
+  findAll() {
+    return this.tasksRepository.find();
+  }
+  findOne(id: number) {
+    return this.tasksRepository.findOne({ where: { id } });
+  }
+  create(body: any) {
+    const newTask = this.tasksRepository.create(body);
+
+    return this.tasksRepository.save(newTask);
+  }
+  async update(id: number, body: any) {
+    const task = await this.tasksRepository.findOne({ where: { id } });
+    this.tasksRepository.merge(task, body);
+    return this.tasksRepository.save(task);
+  }
+  async delete(id: number) {
+    await this.tasksRepository.delete(id);
+    return true;
+  }
+}
